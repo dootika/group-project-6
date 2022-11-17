87a6506f7cc5e60a1477b51546058369b17ecd28
spreet21 <110227705+spreet21@users.noreply.github.com>
2022-11-18 03:35:42 +0530

    Add files via upload

---
+++ Code/Making_std_comp_dataset.R
@@ -0,0 +1,21 @@
+#Studio Analysis
+load("Final_data.RData")
+
+main_dat <- main_dat[!is.na(main_dat$rating), ]
+
+std <- sort(unique(main_dat$Studio))
+std <- std[-1]
+Mean_ratings <- numeric(length(std))
+
+for(i in 1:length(std)){
+  
+  Mean_ratings[i] <- mean(main_dat$rating[which(main_dat$Studio == std[i])])
+}
+
+std_rat <- data.frame(std, Mean_ratings, as.data.frame(table(main_dat$Studio))[-1, 2])
+std_rat <- std_rat[!is.na(Mean_ratings), ]
+
+colnames(std_rat) <- c("Studio Name", "Avg rating of Anime produced", "No. of Anime produced")
+std_rat <- std_rat[which(std_rat[, 3] > 3), ]
+
+save(std_rat, file = "Studio_comparison.RData")
