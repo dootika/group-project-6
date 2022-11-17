0b094f4195ab63b92a61dafa07fd4e402f51603b
shabadiitk <105984634+shabadiitk@users.noreply.github.com>
2022-11-18 02:17:21 +0530

    Creating folders

---
+++ Shiny_App/server.R
@@ -0,0 +1,121 @@
+library(rvest)
+library(shiny)
+load("Genre_vs_Rating(MAL).RData")
+load("Genre.RData")
+load("Final_data.RData")
+
+# Define server logic required to draw a histogram
+shinyServer(function(input, output) {
+    
+  
+   output$histPlot <- renderPlot({
+    if( "All" %in% input$genres){
+      
+      
+        hist(Genre_rat$rating, xlab = "Rating", col = "#007bc2", border = "white",
+             main = "Histogram of Ratings of selected Genres")
+      
+    }
+    else{
+    a <- logical(length(Genre_rat$rating))
+    for(i in 1:length(Genre_rat$genre)){
+      
+      if(sum(input$genres %in% Genre_rat$genre[[i]]) > 0){
+        
+        a[i] <- TRUE
+      }
+    }
+    
+    rating <- Genre_rat$rating[a]
+    
+        hist(rating, xlab = "Rating", col = "#007bc2", border = "white",
+             main = "Histogram of Ratings of selected Genres")
+    }
+  })
+   
+   output$summary <- renderPrint({
+     if( "All" %in% input$genres){
+       
+       cat("Statistics of ratings of selected genres:\n\n")
+       summary(Genre_rat$rating)
+     }else{
+       
+       a <- logical(length(Genre_rat$rating))
+       for(i in 1:length(Genre_rat$genre)){
+         
+         if(sum(input$genres %in% Genre_rat$genre[[i]]) > 0){
+           
+           a[i] <- TRUE
+         }
+       }
+       rating <- Genre_rat$rating[a]
+       
+       cat("Statistics of ratings of selected genres:\n\n")
+       summary(rating)
+     }
+     
+   })
+   
+   output$plot <- renderPlot({
+     
+     genre <- genre[which( (genre$avg_rat <=input$rat[2]) & (genre$avg_rat >=input$rat[1]) ), ]
+     
+     wordcloud(words = genre$genre, freq = genre$Freq, scale = c(3,1),
+               min.freq = input$freq , max.words = input$max ,
+               colors = brewer.pal(8,"Accent"))
+   })
+   
+
+   
+   output$scatt <- renderPlotly({
+     
+     load("Final_data.RData")
+      studios <- unlist(input$studio)
+
+     
+     main_dat <- main_dat[which(main_dat$Release_year %in% c(input$year[1]:input$year[2])), ]
+     
+     if(!("All" %in% studios)){
+
+       main_dat <- main_dat[which(main_dat$Studio %in% unlist(input$studio)), ]
+     }
+     if(!("All" %in% input$type)){
+       main_dat <- main_dat[which(main_dat$type %in% input$type), ]
+     }
+     p <- ggplot(main_dat, aes(text = Name, x = rating, y = members, col = type)) + geom_point()+
+       ylim(input$votes) + xlim(0, 10) + labs(y = "Votes" , x = "Ratings") 
+       #geom_smooth( method="loess", span=0.5)
+       
+     ggplotly(p, width = 1)
+     
+   })
+   
+   
+   output$graph <- renderPlot({
+     
+     load("Final_data.RData")
+     studios <- unlist(input$studio)
+
+     
+     main_dat <- main_dat[which(main_dat$Release_year %in% c(input$year[1]:input$year[2])), ]
+     
+     if(!("All" %in% studios)){
+       
+       main_dat <- main_dat[which(main_dat$Studio %in% unlist(input$studio)), ]
+     }
+     if(!("All" %in% input$type)){
+       main_dat <- main_dat[which(main_dat$type %in% input$type), ]
+     }
+
+     plot(main_dat$rating, main_dat$members,xlim = c(0, 10),
+          ylim = input$votes, col = as.factor(main_dat$type),
+          xlab = "Ratings", ylab = "Votes")
+
+          legend(x = "topleft", legend = sort(unique(main_dat$type)),
+                 fill = 1:length(unique(main_dat$type)))
+         # lines(lowess(main_dat$rating, main_dat$members), col = "red", lwd = 3)
+   })
+
+})
+
+
---
+++ Shiny_App/ui.R
@@ -0,0 +1,97 @@
+library(shiny)
+library(plotly)
+library(wordcloud)
+library(RColorBrewer)
+library(shinythemes)
+
+load("Genre.RData")
+load("Genre_vs_Rating(MAL).RData")
+load("Final_data.RData")
+
+# Define UI for application that draws a histogram
+navbarPage("Anime", theme = shinytheme("darkly"),
+           tabPanel("Plot",
+
+              # Application title
+              titlePanel("Insights on Anime Dataset"),
+
+              # Sidebar with a slider input for number of bins
+              sidebarLayout(
+                  sidebarPanel(
+                    checkboxGroupInput( "genres",
+                                          "Select genres you want to see:",
+                                        c("All", genre$genre), selected = "All"
+                                      )
+                              ),
+
+                 # Show a plot of the generated distribution
+                  mainPanel(
+                    tabsetPanel(
+                    tabPanel("Plot", plotOutput("histPlot")),
+                    tabPanel("Summary", verbatimTextOutput("summary"))
+                              )
+                           )
+                         )
+                      ),
+              tabPanel("Wordcloud",
+                       
+                       titlePanel("Wordcloud of Genres"),
+                       
+                       sidebarLayout(
+                         sidebarPanel(
+                           sliderInput("rat", 
+                                       "Select range of ratings for genres:",
+                                       0, 10, step = 0.1, c(0, 10)),
+                           sliderInput("freq",
+                                       "Minimum Frequency of Anime:",
+                                       min = 1,  max = 2000 , step = 1,  value = 10),
+                           sliderInput("max",
+                                       "Maximum genre:",
+                                       min = 1 ,  max = 40, step = 1, value = 30)
+                         ),
+                         # Show Word Cloud
+                         mainPanel(
+                           plotOutput("plot")
+                         )
+                       )
+                  ),
+           tabPanel("Combined Plot",
+                    
+                    titlePanel("Votes vs Ratings of Anime"),
+                    sidebarLayout(
+                      sidebarPanel(
+                               sliderInput("votes", "Select range of Votes",
+                                           0, 1014000, c(0, 1014000), step = 1000),
+                               sliderInput("year", "Year released", 1907, 2021, value = c(1907, 2021),
+                                           ),
+                              selectizeInput("studio", "Select which studio's anime you want to see:",
+                                             c("All", unique(main_dat$Studio)), selected = "All", multiple = TRUE),
+                               checkboxGroupInput("type", "Select type of anime:",
+                                           c("All", sort(unique(main_dat$type))), selected = "All"),
+                               tags$small(paste0(
+                                 
+                                 "Note:An anime OVA (original video animation) is a special episode or movie that was
+                                 released on DVD/Blu-Ray/VHS without being shown on TV or in theaters before."),
+                                 tags$br(),
+                                 paste0("An original net animation (ONA), known in Japan as web anime, is an anime that
+                                 is directly released onto the Internet."),
+                                 tags$br(),
+                                 paste0("Specials are like OVA's they are not part of origial show but are 
+                                 it's possible for a Special to be released as a television broadcast that
+                                 is just separate from the rest of the show"
+                               ))
+                      ),
+                      mainPanel(
+                             h3("Scatter Plot of number of votes vs ratings :"),
+                             plotOutput("graph"),
+                             h3("Interactive GGplot of the same :"),
+                             plotlyOutput("scatt")
+                      )
+                    ))
+
+          )
+
+
+
+
+
