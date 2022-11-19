b50ec5074c49750614fed985860c0984b6d68b7b
spreet21 <110227705+spreet21@users.noreply.github.com>
2022-11-19 22:22:04 +0530

    Update Scrapping_IMDB.R

--- Code/Scrapping_IMDB.R
+++ Code/Scrapping_IMDB.R
@@ -2,11 +2,10 @@ library(rvest)
 library(tidyverse)
 
 #getting names, ratings, and votes From imdb by web scrapping
-#################
 
 html <- read_html("https://www.imdb.com/search/keyword/?keywords=anime&sort=moviemeter,asc&mode=detail&page=1&ref_=kw_nxt")
 names <- html %>% html_elements("img") %>% html_attr("alt")
-names <- names[-c(1, 2, 53)]
+names <- names[-c(1, 2, 53)]  # removing extra enteries
 
 
 ratings <- html %>% html_elements("strong") %>% html_text()
@@ -16,7 +15,7 @@ ratings <- as.numeric(ratings[-c(1:11)])
 votes <- html %>% html_elements("span") %>% html_attr("data-value")
 votes <- votes[!is.na(votes)]
 votes <- votes[which(votes != "0")]
-votes <- votes[!grepl(",", votes)]
+votes <- votes[!grepl(",", votes)] # removing all those votes with "," as those are dollars in box office( for movie)
 
 for(i in 6:10){
   
@@ -39,7 +38,7 @@ for(i in 6:10){
 }
 votes <- as.integer(votes)
 #removing names with no votes/ratings
-names <- names[-c(59, 78, 260, 265, 324, 354, 410, 419, 465, 471)]
-
+names <- names[-c(59, 78, 260, 265, 324, 354, 410, 419, 465, 471)]   # removing those names whose other data is not available as they are not yet released globally,
+                                                                     # released only in Japan/or upcoming therefore rating and votes doesn't exists
 df <- data.frame(names, ratings, votes)
-save(df, file = "IMDB.RData")
+save(df, file = "IMDB.RData") # saving data frame
