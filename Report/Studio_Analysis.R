503f022c1f79a87fbc06f192c326016f42933c1b
spreet21 <110227705+spreet21@users.noreply.github.com>
2022-11-20 16:38:43 +0530

    Add files via upload

---
+++ Report/Studio_Analysis.R
@@ -0,0 +1,25 @@
+#Studio analysis
+library(plotly)
+load("Studio_comparison.RData") # as std_rat
+
+#Studio and their avg rating
+
+std_rat <- std_rat[order(std_rat[, 3], decreasing = TRUE) ,]
+
+plt <- barplot(std_rat[1:60, 2], col='steelblue', xaxt="n", ylim = c(0, 8), main = "Avg Rating of anime produced")
+text(plt, par("usr")[3], labels = std_rat[1:60, 1], srt = 60, adj = c(1.1,1.1), xpd = TRUE, cex=0.6)
+
+#########
+#Interactive Scatter Plot
+gp <- plot_ly(
+  type = 'scatter',
+  mode = 'markers',
+  x = std_rat[, 2],
+  y = std_rat[, 3],
+  text = std_rat[, 1],
+  hovertemplate = paste('<b> %{text} </b><br>',
+                        'Rating(x axis) : %{x:.2f} <br>',
+                        'Frequency(y axis) : %{y}')
+) %>%
+  layout(title = 'STUDIO analysis', plot_bgcolor = "#e5ecf6", xaxis = list(title = "Avg rating of Anime produced"), 
+         yaxis = list(title = "No. of Anime produced"))
